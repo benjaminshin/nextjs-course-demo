2320c730cfa6bcce2bbbc0738d3e40963d332aca
benjaminshin <67225825+benjaminshin@users.noreply.github.com>
2022-08-24 21:31:17 +0900

    ready for deployment

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,74 @@
+
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+import {MongoClient, ObjectId} from "mongodb";
+import {Fragment} from 'react';
+import Head from 'next/head';
+
+
+function MeetupDetails(props){
+
+    return (
+        <Fragment>
+<Head>
+<title>{props.meetupData.title}</title>
+<meta 
+name='description'
+content={props.meetupData.description}
+/>
+</Head>
+    <MeetupDetail 
+    image={props.meetupData.image}
+    title={props.meetupData.title}
+    address={props.meetupData.address}
+    description={props.meetupData.description}
+    />
+
+        </Fragment>
+        
+    
+    );
+}
+
+export async function getStaticPaths(){
+    const client= await MongoClient.connect('mongodb+srv://benjaminshin:shs158792@cluster0.qukwx.mongodb.net/meetups?retryWrites=true&w=majority');
+    const db= client.db();
+    const meetupsCollection= db.collection('meetups');
+    
+    const meetups= await meetupsCollection.find({}, {_id: 1}).toArray();
+
+    client.close();
+    return {
+        fallback:false ,
+        paths:meetups.map(meetup=>({
+            params:{meetupId:meetup._id.toString()}
+        })), 
+    };
+};
+
+
+export async function getStaticProps(context){
+const meetupId= context.params.meetupId;
+console.log(meetupId);
+const client= await MongoClient.connect('mongodb+srv://benjaminshin:shs158792@cluster0.qukwx.mongodb.net/meetups?retryWrites=true&w=majority');
+    const db= client.db();
+    const meetupsCollection= db.collection('meetups');
+    
+    const selectedMeetup= await meetupsCollection.findOne({_id: ObjectId(meetupId)});
+
+    client.close();
+
+    return {
+props:{
+    meetupData: {
+        id:selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        image: selectedMeetup.image,
+        address: selectedMeetup.address,
+        description:selectedMeetup.description,
+    },
+},
+
+    };
+};
+
+export default MeetupDetails;
